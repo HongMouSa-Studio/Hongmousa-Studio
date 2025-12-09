3a5ca4cee4bde1717fbeecdb8313a48ca153f007
HongMouSa-Studio <hongmousa.studio@gmail.com>
2025-12-09 19:36:40 +0800

    update content

---
+++ content/tb-poj/_index.md
@@ -0,0 +1,8 @@
+---
+title: "Hong-mô͘-sa Kang-chok-sek"
+---
+
+<section class="hero">
+  <h1>芳芼莎工作室</h1>
+  <p>文創品牌、手工產品、理念分享</p>
+</section>
--- public/js/menu.js
+++ public/js/menu.js
@@ -23,3 +23,12 @@
 		menu.addEventListener('transitionend', removeMenuTransition, false);
 	}
 }(document, window));
+
+window.addEventListener('scroll', () => {
+    const header = document.querySelector('header');
+    if(window.scrollY > 50) {
+        header.classList.add('scrolled');
+    } else {
+        header.classList.remove('scrolled');
+    }
+});
